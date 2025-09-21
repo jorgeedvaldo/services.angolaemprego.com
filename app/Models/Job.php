0c0dea4baa0dad28589019c01c73d7c4ada64d51
jorgeedvaldo <edvaldojorge1996@gmail.com>
2025-09-21 23:15:32 +0100

    Add fillable attributes to Job model for improved mass assignment

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Job extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title', 'slug', 'company', 'location', 'description', 'email_or_link', 'image'
+    ];
 }
