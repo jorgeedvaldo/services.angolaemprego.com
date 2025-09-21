00eb21692360f7243c272e34f0365ff03d7ed173
jorgeedvaldo <edvaldojorge1996@gmail.com>
2025-09-21 20:59:36 +0100

    Refactor social media posting functionality by consolidating Facebook and LinkedIn services into controllers, and implement job posting methods in SocialPostController.

---
+++ app/Http/Controllers/FacebookController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\SocialMedia\FacebookService
+
+class FacebookController extends Controller
+{
+    protected FacebookService $facebookService;
+
+    public function __construct(protected FacebookService $facebookService)
+    {
+        $this->facebookService = $facebookService;
+    }
+    public function post(string $message, string $link)
+    {
+        $this->facebookService->post($message, $link);
+    }
+}
--- app/Http/Controllers/SocialPostController.php
+++ app/Http/Controllers/SocialPostController.php
@@ -3,8 +3,49 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\FacebookController;
+use App\Http\Controllers\LinkedInController;
+use App\Models\Job;
 
 class SocialPostController extends Controller
 {
-    //
+    protected FacebookController $facebookController;
+    protected LinkedInController $linkedInController;
+
+    public function __construct(
+        FacebookController $facebookController,
+        LinkedInController $linkedInController
+    ) {
+        $this->facebookController = $facebookController;
+        $this->linkedInController = $linkedInController;
+    }
+
+    public function postToSocialMedia(Job $job)
+    {
+        $link    = "https://www.angolaemprego.com/vagas/" . $job->slug;
+        $message = $job->title . "\n.\nMais detalhes aqui: " . $link . "\n.";
+
+        // Post to Facebook
+        $this->facebookController->post($message, $link);
+
+        // Post to LinkedIn
+        if ($link) {
+            $this->linkedInController->publishLink($message, $link);
+        } else {
+            $this->linkedInController->publishText($message);
+        }
+
+        return response()->json(['status' => 'Posts submitted']);
+    }
+
+    public function LimparDescricao($Text)
+    {
+		$NovaDescricao = str_replace("<br>", "\n<br>", $Text);
+		$NovaDescricao = str_replace(["</p>", "</h1>", "</h2>", "</h3>", "</li>"], ["</p>\n", "</h1>\n", "</h2>\n", "</h3>\n", "</li>\n"], $NovaDescricao);
+		$NovaDescricao = explode('----------', $NovaDescricao)[0];
+		$NovaDescricao = strip_tags($NovaDescricao);
+		$NovaDescricao = str_replace("&nbsp;", "", $NovaDescricao);
+
+		return $NovaDescricao;
+    }
 }
