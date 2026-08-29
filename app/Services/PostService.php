45032bbc1a41ee45521d7a082951c429a43b4365
Edivaldo Jorge <32965474+jorgeedvaldo@users.noreply.github.com>
2026-08-29 09:55:59 +0000

    fix: Update API endpoint in PostService to correct URL

--- app/Services/PostService.php
+++ app/Services/PostService.php
@@ -21,7 +21,7 @@ class PostService
     public function fetchFromWebsite()
     {
         try {
-            $response = $this->http->get('https://clickpetroleoegas.com.br/rna.ao/wp-json/wp/v2/posts');
+            $response = $this->http->get('https://rna.ao/rna.ao/wp-json/wp/v2/posts');
         } catch (\Exception $e) {
             \Log::error('Falha na requisição HTTP para clickpetroleoegas: ' . $e->getMessage());
             return;
